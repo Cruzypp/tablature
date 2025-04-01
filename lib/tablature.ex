762121f6fa88eeec07b5f027bb3aeaa51e2fdd53
Cruz P <A01738823@tec.mx>
2025-04-01 15:51:57 -0600

    Done with stairway to heaven's test

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,16 +1,21 @@
 defmodule Tablature do
   def parse(tab) do
-    tab |> String.split()
-    |> Enum.map(fn line -> parse_line(line) end)
-    |> Enum.filter(fn l -> length(l) > 0 end)
-    |> Enum.zip
-    |> Enum.map(fn t -> Tuple.to_list(t) end)
-    |> List.flatten
+    tab
+    |> String.split()
+    |> Enum.map(fn t -> parse_line(t) end)              # Parsea cada línea
+    |> List.flatten() #Une las listas de tuplas
+    |> Enum.sort_by(fn {_num, index} -> index end, :asc) # Ordena por índice
+    |> Enum.map(fn {num, _i} -> num end) # Quita el índice
     |> Enum.join(" ")
   end
 
   def parse_line(line) do
     letter = String.at(line, 0)
-    line |> String.graphemes |> Enum.filter(fn c -> c =~ ~r/\d/ end) |> Enum.map(fn d -> letter <> d end)
+
+    line
+    |> String.graphemes() # Se divide en caracteres
+    |> Enum.with_index() # Se crea una tupla con el carácter y su índice
+    |> Enum.filter(fn {char, i} -> if char =~ ~r/\d/ do {char, i} end end)   # Filtra los dígitos
+    |> Enum.map(fn {num, i} -> {letter <> num, i} end)  # Añade la letra a cada dígito
   end
 end
